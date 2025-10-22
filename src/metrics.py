ce578a13cd4ec2698f7d3a3bc9a1e9435c364f11
Nadine <nadineknoop@hotmail.com>
2025-10-22 15:19:21 +0200

    började med import och notebook

---
+++ src/metrics.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/ecommerce_sales.csv")
+
+def get_df():
+    return df
+
+
+
+# Kod till 2. var säljer vi? vilka städer står för störst intäkt?
+revenue_per_city = df.groupby('city')[['revenue']].sum()
+
+top_cities = revenue_per_city.sort_values(by='revenue', ascending=False)
+
+units_per_city = df.groupby(['city', 'category'])['units'].sum().unstack()
+
+#
