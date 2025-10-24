4bb59154e57a2cdd8d994c547e7f0c4ff0f299f4
Bilal Mah <bilalking@live.se>
2025-10-24 17:23:56 +0200

    fixat lite på 3an

--- src/metrics.py
+++ src/metrics.py
@@ -8,19 +8,15 @@ def get_df():
     return df
 
 # #Kod till 1. vad säljer vi?
-# unique_products = df["category"].unique()
-
-# category_revenue = df.groupby("category")["revenue"].sum().sort_values(ascending=False)
+unique_products = df["category"].unique()
+category_revenue = df.groupby("category")["revenue"].sum().sort_values(ascending=False)
 
 
 # # Kod till 2. var säljer vi? vilka städer står för störst intäkt?
-# revenue_per_city = df.groupby('city')[['revenue']].sum()
-
-# top_cities = revenue_per_city.sort_values(by='revenue', ascending=False)
-
-# units_per_city = df.groupby(['city', 'category'])['units'].sum().unstack()
-
-# category_per_city = df.groupby(['city', 'category'])['revenue'].sum().unstack()
+revenue_per_city = df.groupby('city')[['revenue']].sum()
+top_cities = revenue_per_city.sort_values(by='revenue', ascending=False)
+units_per_city = df.groupby(['city', 'category'])['units'].sum().unstack()
+category_per_city = df.groupby(['city', 'category'])['revenue'].sum().unstack()
 
 # 
 
@@ -39,3 +35,5 @@ print(revenue_by_month)
 
 
 
+def get_revenue_by_month():
+    return revenue_by_month
