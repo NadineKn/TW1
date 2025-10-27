36b974bfeec676c1b0789bf991b9b424524712ff
iidaclaesson <iidaclaesson@gmail.com>
2025-10-27 20:26:03 +0100

    Kod och visualisering till fråga 4

--- src/viz.py
+++ src/viz.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-=======
-=======
->>>>>>> main
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -21,7 +17,7 @@ plt.xlabel("Category")
 plt.ylabel("Revenue")
 plt.grid(True, linestyle='-', linewidth=0.5)
 plt.tight_layout()
-<<<<<<< HEAD
+
 plt.show()
 
 # 3, när säljer vi?
@@ -45,6 +41,23 @@ plt.grid(True)
 plt.tight_layout()
 plt.show()
 
+# 4, Hur ser en typisk order ut? AOV och spridning
+
+# Average order value and distribution
+fig, (ax_hist, ax_box) = plt.subplots(2, 1, figsize=(10, 6), gridspec_kw={"height_ratios":[4,1]})
+
+ax_hist.hist(df["revenue"], bins=50)
+ax_hist.set_title("Distribution of Order Values")
+
+ax_hist.set_ylabel("Number of Orders")
+ax_hist.grid(True, axis="y")
+
+ax_box.boxplot(df["revenue"], vert=False)
+ax_box.set_xlabel("Order Value (SEK)")
+ax_box.set_yticklabels("")
+ax_box.grid(True, axis="x")
+plt.tight_layout()
+
 
 #5. Top-3 kategorier
 
@@ -62,7 +75,6 @@ plt.ylabel("Total omsättning")
 plt.gca().yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
 
 plt.show()
->>>>>>> Stashed changes
-=======
+
 plt.show()
->>>>>>> main
+
